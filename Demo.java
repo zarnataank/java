b19ffdad94447e9f0616bfb1d143ec895da32687
zarnataank <129717592+zarnataank@users.noreply.github.com>
2023-08-22 13:25:52 +0530

    dd
    ddd

---
+++ Demo.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class Demo
+{
+public static void main(String PD[])
+{
+	char c;
+Scanner k=new Scanner(System.in);
+System.out.print("enter a character:");
+ c=k.next().charAt(0); //index of char
+int a=(int)c; //typecasting
+System.out.println(a);
+}
+}
+
